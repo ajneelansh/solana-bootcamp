01325d9899175a57f165c3683d89fe7e0c1e4176
ajneelansh <ajneelansh@gmail.com>
2025-10-28 12:30:28 +0530

    Object-Oriented in Rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+struct Car {
+    model: String,
+    year: u32
+}
+impl Car {
+    fn calculate_price(&self) -> u32 {
+        let i;
+        if self.model=="S" {
+            i = 50000
+        }
+        else {
+            i = 40000
+        } 
+        return i*self.year; 
+    }
+}
+
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let lambo : Car = Car {
+       model : String::from("S"),
+       year : 2003
+    };
+
+    print!("{}",lambo.calculate_price());
 }
