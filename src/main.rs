11fb0ad104e03b21ceedeb9892a6fa672ab66137
ajneelansh <ajneelansh@gmail.com>
2025-10-30 14:05:19 +0530

    Building Custom Derive Macros

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,57 @@
-struct Car {
-    model: String,
-    year: u32
+use std::fmt::Error;
+
+#[derive(Debug)]
+struct Swap {
+    a: u32,
+    b: u32
 }
-impl Car {
-    fn calculate_price(&self) -> u32 {
-        let i;
-        if self.model=="S" {
-            i = 50000
+
+trait Serialize {
+    fn serialize(&self) -> Vec<u8>;
+}
+
+trait Deserialise {
+    fn deserialise(v:&[u8]) ->  Result<Swap,Error>;
+}
+
+impl Deserialise for Swap {
+    fn deserialise(v: &[u8]) ->  Result<Swap,Error> {
+        if v.len() < 8{
+            return Err(Error);
         }
         else {
-            i = 40000
-        } 
-        return i*self.year; 
+          let a = u32::from_be_bytes([v[0],v[1],v[2],v[3]]);
+          let b= u32::from_be_bytes([v[4],v[5],v[6],v[7]]);
+
+          return Ok(Swap {
+           a:a,
+           b:b
+
+          });
+        }
     }
 }
 
+impl Serialize for Swap {
+    fn serialize(&self) -> Vec<u8>{
 
+       let mut v = vec![];
+       v.extend_from_slice(&self.a.to_be_bytes());
+       v.extend_from_slice(&self.b.to_be_bytes());
+       return v;
 
+    }
+}
 
-fn main() {
-    let lambo : Car = Car {
-       model : String::from("S"),
-       year : 2003
+fn main(){
+    let s = Swap{
+        a: 2,
+        b:4
     };
+    let v = s.serialize();
+    print!("{:?}",v);
 
-    print!("{}",lambo.calculate_price());
+    let x = Swap::deserialise(&v).unwrap();
+    print!("{:?}",x);
+
 }
